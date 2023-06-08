1f1c01969f373b3e80f9c31bc8ce26908c167b8f
Cristiano <cristianojr0101@gmail.com>
2023-06-07 21:46:55 -0300

    Criação de Cliente

---
+++ Configuracao/DAL/ClienteDAL.cs
@@ -0,0 +1,197 @@
+﻿using ClassLibrary1;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ClienteDAL
+    {
+        public void Inserir(Cliente _cliente)
+        {
+            SqlConnection cn = new SqlConnection();
+
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"INSERT INTO Cliente(Nome, CPF, RG, Email, Telefone)
+                                  VALUES (@Nome, @CPF, @RG, @Email, @Telefone)";
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", _cliente.Nome);
+                cmd.Parameters.AddWithValue("@CPF", _cliente.CPF);
+                cmd.Parameters.AddWithValue("@RG", _cliente.RG);
+                cmd.Parameters.AddWithValue("@Email", _cliente.Email);
+                cmd.Parameters.AddWithValue("@Telefone", _cliente.Telefone);
+             
+
+                cn.Open();
+                cmd.ExecuteScalar();
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um cliente no banco de dados. " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    
+        public List<Cliente> BuscarTodos()
+        {
+            
+                List<Cliente> clienteList = new List<Cliente>();
+                Cliente cliente;
+                SqlConnection cn = new SqlConnection();
+                SqlCommand cmd = new SqlCommand();
+
+                try
+
+                {
+                    cn.ConnectionString = Conexao.StringDeConexao;
+                    cmd.Connection = cn;
+                    cmd.CommandText = @"SELECT Id, Nome, CPF, RG, Email, Telefone FROM Cliente";
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cn.Open();
+
+                    using (SqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            cliente = new Cliente();
+                            cliente.Id = Convert.ToInt32(rd["Id"]);
+                            cliente.Nome = rd["Nome"].ToString();
+                            cliente.CPF = rd["CPF"].ToString();
+                            cliente.RG = rd["RG"].ToString();
+                            cliente.Email = rd["Email"].ToString();
+                            cliente.Telefone = rd["Email"].ToString();
+
+                            clienteList.Add(cliente);
+                        }
+                    }
+                    return clienteList;
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception("Ocorreu um erro ao tentar buscar todos os clientes no banco de dados. " + ex.Message);
+                }
+                finally
+                {
+                    cn.Close();
+                }
+
+
+            
+        }
+        public Cliente BuscarPorNome(string _nomeCliente)
+        {
+            Cliente cliente = new Cliente();
+            SqlConnection cn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT Id, Nome, CPF, RG, Email, Telefone FROM Cliente WHERE Nome like @Nome";
+                cmd.Parameters.AddWithValue("@NomeUsuario", "%"+_nomeCliente+"%");
+                cmd.CommandType = System.Data.CommandType.Text;
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        cliente = new Cliente();
+                        cliente.Id = Convert.ToInt32(rd["Id"]);
+                        cliente.Nome = rd["Nome"].ToString();
+                        cliente.CPF = rd["CPF"].ToString();
+                        cliente.RG = rd["RG"].ToString();
+                        cliente.Email = rd["Email"].ToString();
+                        cliente.Telefone = rd["Telfone"].ToString();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw; new Exception("Ocoreu um erro ao tentar fazer busca por nome de clientes no banco de dados. ");
+            }
+
+            return cliente;
+
+        }
+        public Cliente BuscarPorId(int _id)
+        {
+
+            SqlConnection cn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            Cliente cliente = new Cliente();
+
+            try
+
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT Id, Nome, CPF, RG, Email, Telefone FROM Cliente WHERE Id = @Id";
+                cmd.Parameters.AddWithValue("@Id", _id);
+                cmd.CommandType = System.Data.CommandType.Text;
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        cliente = new Cliente();
+                        cliente.Id = Convert.ToInt32(rd["Id"]);
+                        cliente.Nome = rd["Nome"].ToString();
+                        cliente.CPF = rd["CPF"].ToString();
+                        cliente.RG = rd["RG"].ToString();
+                        cliente.Email = rd["Email"].ToString();
+                        cliente.Telefone = rd["Telefone"].ToString();
+
+
+                      
+
+                    }
+                }
+                return cliente;
+
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine(String.Format("Ocorreu o seguinte erro: {0} ao tentar buscar no banco "));
+
+                throw new Exception("Ocorreu um erro ao tentar buscar um usuário: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public Cliente BuscarPorCPF(string _CPF)
+        {
+            return null ;
+        }
+        public void Alterar(Cliente _cliente)
+        {
+            
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+        
+    }
+}
--- Configuracao/WindowsFormsAppPrincipal/FormPrincipal.Designer.cs
+++ Configuracao/WindowsFormsAppPrincipal/FormPrincipal.Designer.cs
@@ -31,7 +31,6 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.permissõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.grupoDeUsuáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.fornecedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -53,7 +52,6 @@
             // 
             this.cadastroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.usuariosToolStripMenuItem,
-            this.permissõesToolStripMenuItem,
             this.grupoDeUsuáriosToolStripMenuItem,
             this.clientesToolStripMenuItem,
             this.fornecedoresToolStripMenuItem});
@@ -64,33 +62,27 @@
             // usuariosToolStripMenuItem
             // 
             this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
-            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.usuariosToolStripMenuItem.Text = "Usuários";
             this.usuariosToolStripMenuItem.Click += new System.EventHandler(this.usuariosToolStripMenuItem_Click);
-            // 
-            // permissõesToolStripMenuItem
-            // 
-            this.permissõesToolStripMenuItem.Name = "permissõesToolStripMenuItem";
-            this.permissõesToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
-            this.permissõesToolStripMenuItem.Text = "Permissões";
             // 
             // grupoDeUsuáriosToolStripMenuItem
             // 
             this.grupoDeUsuáriosToolStripMenuItem.Name = "grupoDeUsuáriosToolStripMenuItem";
-            this.grupoDeUsuáriosToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.grupoDeUsuáriosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.grupoDeUsuáriosToolStripMenuItem.Text = "Grupo de usuários";
             this.grupoDeUsuáriosToolStripMenuItem.Click += new System.EventHandler(this.grupoDeUsuáriosToolStripMenuItem_Click);
             // 
             // clientesToolStripMenuItem
             // 
             this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
-            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.clientesToolStripMenuItem.Text = "Clientes";
             // 
             // fornecedoresToolStripMenuItem
             // 
             this.fornecedoresToolStripMenuItem.Name = "fornecedoresToolStripMenuItem";
-            this.fornecedoresToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.fornecedoresToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.fornecedoresToolStripMenuItem.Text = "Fornecedores";
             // 
             // FormPrincipal
@@ -119,7 +111,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem cadastroToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem permissõesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem grupoDeUsuáriosToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem fornecedoresToolStripMenuItem;
